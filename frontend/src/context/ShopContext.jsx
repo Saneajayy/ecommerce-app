3d61af18d2f97337ae10b44c942b9db1f1d3670a
ajay <ajaykumarrj302@gmail.com>
2026-01-13 23:11:52 +0530

    added latest products in the home page

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import { createContext } from "react";
 import { products } from "../assets/assets";
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
