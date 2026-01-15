9383d73913be5a7e884fcfb65fc40e4cd61f33b1
ajay <ajaykumarrj302@gmail.com>
2026-01-15 19:53:46 +0530

    implemented Search bar component in the collection page

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -6,7 +6,7 @@ import ProductItem from '../components/ProductItem';
 
 const Collection = () => {
 
-  const { products } = useContext(ShopContext);
+  const { products , search , showSearch } = useContext(ShopContext);
   const [showFilter,setShowFilter] = useState(false);
   const [filterProducts,setFilterProducts] = useState([]);
   const [category,setCategory] = useState([]);
@@ -34,6 +34,10 @@ const Collection = () => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const applyFilter = () => {
     let productsCopy = products.slice();
+
+    if (showSearch && search){
+      productsCopy = productsCopy.filter(item => item.name.toLowerCase().includes(search.toLowerCase()))
+    }
 
     if(category.length > 0) {
       productsCopy = productsCopy.filter(item => category.includes(item.category))
@@ -65,13 +69,13 @@ const Collection = () => {
   }
 
   useEffect(() => {
-    // eslint-disable-next-line react-hooks/set-state-in-effect
+     
     applyFilter();
-  },[applyFilter, category, subCategory])
+  },[category, subCategory,search,showSearch])
 
   useEffect(() => {
     sortProduct();
-  },[sortProduct, sortType])
+  },[sortType])
 
 
   return (
