75a66d5da86d636fe7888bdc115cbe7f7377a6c6
ajay <ajaykumarrj302@gmail.com>
2026-01-25 19:20:47 +0530

    add to cart logic

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
 import userRouter from './routes/userRoutes.js';
 import productRouter from './routes/productRoute.js';
+import cartRouter from './routes/cartRoute.js';
 
 // App config
 
@@ -17,7 +18,7 @@ connectCloudinary()
 
 app.use(express.json())
 app.use(cors())
-
+app.use('/api/cart',cartRouter)
 
 // api endpoints
 app.use('/api/user',userRouter)
