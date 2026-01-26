5a07eab3b55d7829c315d29df95a7b0b95128c2e
ajay <ajaykumarrj302@gmail.com>
2026-01-26 14:23:45 +0530

    initialized razorpay  payment method

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -2,9 +2,14 @@ import orderModel from "../models/orderModel.js";
 import productModel from "../models/productModel.js";
 import userModel from "../models/userModel.js";
 import Stripe from "stripe";
+import razopay from 'razorpay'
 
 // GATEWAY INITIALIZE 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+const razorpayInstance = new newrazorpay({
+    key.id : process.env.RAZORPAY_KEY_ID,
+    key_secret : process.env.RAZORPAY_KEY_SECRET,
+})
 
 // global variables
 
@@ -59,7 +64,7 @@ const placeOrderStripe = async (req,res) => {
             items,
             address,
             amount,
-            paymentMethod: 'COD',
+            paymentMethod: 'Stripe',
             payment:false,
             date:Date.now()
         }
@@ -130,7 +135,61 @@ const verifyStripe = async (req,res) => {
 // placing order using Razorpay
 
 const placeOrderRazorpay = async (req,res) => {
-    
+    try {
+
+        const {userId,items,amount,address} = req.body
+
+
+        const orderData = {
+            userId,
+            items,
+            address,
+            amount,
+            paymentMethod: 'Razorpay',
+            payment:false,
+            date:Date.now()
+        }
+
+        const newOrder = new orderModel(orderData)
+        await newOrder.save()
+
+        const options = {
+            amount: amount*100,
+            currency: currency.toUpperCase(),
+            receipt: newOrder._id.toString()
+        }
+
+        await razorpayInstance.orders.create(options, (error,order)=>{
+            if (error) {
+                console.log(error)
+                return res.json({success:false,message:error})
+            } 
+            res.json({success:true,order})
+        })
+        
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:error.message})
+    }
+}
+
+const verifyRazorpay = async (req,res) => {
+    try {
+
+        const {userId,razorpay_order_id} = req.body
+        const orderInfo = await razorpayInstance.orders.fetch(razorpay_order_id)
+        if (orderInfo.status === 'paid') {
+            await orderModel.findByIdAndUpdate(orderInfo.receipt,{payment:true});
+            await userModel.findByIdAndUpdate(userId,{cartData:{}})
+            res.json({success:true,message:'Payment Successful'})
+        } else {
+            res.json({success:false,message:'Payment Failed'})
+        }
+        
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:error.message})
+    }
 }
 
 // All orders data for admin panel 
@@ -179,4 +238,4 @@ const updateStatus = async (req,res) => {
     
 }
 
-export {verifyStripe,placeOrder,placeOrderRazorpay,placeOrderStripe,allOrders,userOrders,updateStatus}
+export {verifyRazorpay,verifyStripe,placeOrder,placeOrderRazorpay,placeOrderStripe,allOrders,userOrders,updateStatus}
--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -5,6 +5,7 @@ import { assets } from '../assets/assets'
 import { ShopContext } from '../context/ShopContext'
 import axios from 'axios'
 import { toast } from 'react-toastify'
+import { currency } from '../../../admin/src/App'
 
 const PlaceOrder = () => {
 
@@ -29,6 +30,37 @@ const PlaceOrder = () => {
     const value = event.target.value;
 
     setFormData(data => ({...data,[name]:value}))
+  }
+
+  const initPay = (order) => {
+    const options = {
+      key: import.meta.env.VITE_RAZORPAY_KEY_ID,
+      amount: order.amount,
+      currency: order.currency,
+      name:'Order Payment',
+      description:'Order Payment'
+      order_id: order.id,
+      receipt: order.receipt,
+      handler: async (resonse) => {
+        console.log(response)
+        try {
+
+          const { data } = await axios.post(backendUrl + '/api/order/verifyRazorpay',response,{headers:{token}});
+          if (data.success) {
+            navigate('/orders')
+            setCartItems({})
+          }
+          
+        } catch (error) {
+          console.log(error)
+          toast.error(error)
+        }
+      }
+
+    }
+
+    const rzp = new window.Razorpay(options)
+    rzp.open()
   }
 
   const onSubmitHandler = async (event) => {
@@ -80,9 +112,16 @@ const PlaceOrder = () => {
             } else {
               toast.error(responseStripe.data.message)
             }
+            break;
 
+            case 'razorpay' :
 
-          break;
+            const responseRazorpay = await axios.post(backendUrl + '/api/order/razorpay', orderData,{headers:{token}})
+            if (responseRazorpay.data.success) {
+              initPay(responseRazorpay.data.order);
+            }
+
+            break;
 
         default:
           break;
