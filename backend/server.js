62cef49de2c7eaf45fe5c3a10fa196bdbaa17211
ajay <ajaykumarrj302@gmail.com>
2026-01-26 22:02:55 +0530

    bug

--- backend/server.js
+++ backend/server.js
@@ -18,13 +18,7 @@ connectCloudinary()
 // middlewares
 
 app.use(express.json())
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://ecommerce-frontend-one-orcin.vercel.app");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
-
+app.use(cors())
 app.use('/api/cart',cartRouter)
 
 // api endpoints
