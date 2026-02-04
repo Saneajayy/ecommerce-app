5f11255c07d8d8c9d3c1974f538c9ad6daa61e3a
ajay <ajaykumarrj302@gmail.com>
2026-02-04 19:10:47 +0530

    commit

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -9,7 +9,7 @@ const orderSchema = new mongoose.Schema({
     paymentMethod: {type:String,required:true},
     payment: {type:Boolean,required:true,default:false},
     date:{type:Number,required:true}
-
+// ddd
 
 })
 
